47e2ee26ce48ca38c8d294feb06e4887f7fbc826
OliWan9 <oliver.hitz.7@gmail.com>
2025-12-23 11:11:51 +0100

    give parameters to print()

--- main.go
+++ main.go
@@ -33,25 +33,22 @@ func getData() (*disk.UsageStat, []cpu.InfoStat, []float64, *mem.VirtualMemorySt
 	return diskUsage, cpuInfo, cpuPercent, memoryInfo, nil
 }
 
-func print() {
-	for {
-		diskUsage, cpuInfo, cpuPercent, memoryInfo, err := getData()
-
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Println("---System Monitor---")
-		fmt.Println("CPU Model:", cpuInfo[0].ModelName)
-		fmt.Printf("CPU Usage: %.2f%%\n", cpuPercent[0])
-		fmt.Printf("Disk Used: %.2f%%\n", diskUsage.UsedPercent)
-		fmt.Printf("Memory Used: %.2f%%\n", memoryInfo.UsedPercent)
-
-		// Wait 2 seconds before next update
-		time.Sleep(2 * time.Second)
+func print(diskUsage *disk.UsageStat, cpuInfo []cpu.InfoStat, cpuPercent []float64, memoryInfo *mem.VirtualMemoryStat, err error) {
+	if err != nil {
+		fmt.Println(err)
 	}
+
+	fmt.Println("---System Monitor---")
+	fmt.Println("CPU Model:", cpuInfo[0].ModelName)
+	fmt.Printf("CPU Usage: %.2f%%\n", cpuPercent[0])
+	fmt.Printf("Disk Used: %.2f%%\n", diskUsage.UsedPercent)
+	fmt.Printf("Memory Used: %.2f%%\n", memoryInfo.UsedPercent)
+
+	// Wait 2 seconds before next update
+	time.Sleep(2 * time.Second)
 }
 
 func main() {
-	print()
+	diskUsage, cpuInfo, cpuPercent, memoryInfo, err := getData()
+	print(diskUsage, cpuInfo, cpuPercent, memoryInfo, err)
 }
